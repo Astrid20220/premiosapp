45365dea7fa94808358aa1307fca9988db18f7d3
Astrid Gonzalez <yadiragonzalez556@gmail.com>
2022-08-02 22:38:13 -0500

    mejorando el admin

--- Desktop/curso/premiosapp/polls/admin.py
+++ Desktop/curso/premiosapp/polls/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiseInLine(admin.StackedInline):
+    model = Choise
+    extra: 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiseInLine]    
+
+admin.site.register(Question, QuestionAdmin)
+
